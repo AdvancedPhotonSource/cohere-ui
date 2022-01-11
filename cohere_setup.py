0246f50981e383f86c3cd2a897c9fd0f208524aa
Barbara Frosik <bfrosik@anl.gov>
2022-01-11 17:58:41 -0600

    Added finding correction files

--- cohere_setup.py
+++ cohere_setup.py
@@ -4,33 +4,66 @@ import argparse
 from shutil import copy
 import re
 import time
+import glob
 
 
 def getspecfile_datetime(file):
     SPEC_datetime = re.compile(r"^#D")
-    SPEC_time_format = re.compile(r"\d\d:\d\d:\d\d")
-    SPEC_multi_blank = re.compile(r"\s+")
-    datetimestruct = None
+    datestr = None
 
     datetimeformat = "%a %b %d %H:%M:%S %Y"
     with open(file) as fid:
         for line in fid:
             if SPEC_datetime.match(line):
-                filetime = SPEC_time_format.findall(line)[0]
-                line = SPEC_datetime.sub("", line)
-                line = SPEC_multi_blank.sub(" ", line).strip()
-                # print(line)
+                line = SPEC_datetime.sub("", line).strip()
                 datetimestruct = time.strptime(line, datetimeformat)
+                mon = datetimestruct.tm_mon
+                if mon < 10:
+                    mon = '0' + str(mon)
+                else:
+                    mon = str(mon)
+                mday = datetimestruct.tm_mday
+                if mday < 10:
+                    mday = '0' + str(mday)
+                else:
+                    mday = str(mday)
+                datestr = str(datetimestruct.tm_year) + mon + mday
                 break
 
-    return datetimestruct
+    return datestr
+
+
+def get_dark(detcorrectionsdir, spec_timestamp):
+    files = glob.glob1(detcorrectionsdir, '*darkfield.tif')
+    files.sort()
+    filestamps = [file[8:16] for file in files]
+    filestamps.append(spec_timestamp)
+    filestamps.sort()
+    ind = filestamps.index(spec_timestamp)
+    # check for case the stamp is the same
+    if len(filestamps) >= ind and filestamps[ind] == filestamps[ind + 1]:
+        return (os.path.join(detcorrectionsdir, files[ind]))
+    elif ind == 0:
+        return ""
+    return(os.path.join(detcorrectionsdir, files[ind-1]))
+
+
+def get_white(detcorrectionsdir, spec_timestamp):
+    files = glob.glob1(detcorrectionsdir, '*whitefield.tif')
+    files.sort()
+    filestamps = [file[8:16] for file in files]
+    filestamps.append(spec_timestamp)
+    filestamps.sort()
+    ind = filestamps.index(spec_timestamp)
+    # check for case the stamp is the same
+    if len(filestamps) >= ind and filestamps[ind] == filestamps[ind + 1]:
+        return (os.path.join(detcorrectionsdir, files[ind]))
+    elif ind == 0:
+        return ""
+    return(os.path.join(detcorrectionsdir, files[ind-1]))
 
 
 def setup(script_dir, working_dir, det_name, specfile):
-    # spec_timestamp = getspecfile_datetime(specfile)
-    # spec_date = str(spec_timestamp.tm_year) + str(spec_timestamp.tm_mon) + str(spec_timestamp.tm_mday)
-    # print(spec_date)
-
     exp_data_dir, spec_filename = os.path.split(specfile)
     specfilebase = os.path.splitext(spec_filename)[0]
     addatadir = os.path.join(exp_data_dir, "AD" + det_name + "_" + specfilebase)
@@ -56,8 +89,9 @@ def setup(script_dir, working_dir, det_name, specfile):
     detcorrectionsdir = os.path.join(script_dir, 'cohere-scripts', 'beamlines', 'aps_34idc', 'detector_corrections', det_name)
     # figure out the dark and white files
     if os.path.isdir(detcorrectionsdir):
-        darkfieldfile = os.path.join(detcorrectionsdir, "current_dark.tif")
-        whitefieldfile = os.path.join(detcorrectionsdir, "current_white.tif")
+        spec_timestamp = getspecfile_datetime(specfile)
+        darkfieldfile = get_dark(detcorrectionsdir, spec_timestamp)
+        whitefieldfile = get_white(detcorrectionsdir, spec_timestamp)
     else:
         darkfieldfile = ""
         whitefieldfile = ""
