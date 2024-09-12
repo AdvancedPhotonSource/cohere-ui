061b5eff77fc6ba83f8250fe21a74d2d4a3696db
Barbara Frosik <bfrosik@anl.gov>
2024-09-12 09:36:15 -0500

    Update preprocessor.py to commit to new interface

--- cohere-scripts/beamlines/esrf_id01/preprocessor.py
+++ cohere-scripts/beamlines/esrf_id01/preprocessor.py
@@ -3,10 +3,9 @@ import numpy as np
 from multiprocessing import Pool, Process, Queue
 import cohere_core.utilities as ut
 import cohere_core.utilities.dvc_utils as dvut
-from functools import partial
 
 
-def report_corr_err(q, ref_scan, dir_no, save_dir):
+def report_corr_err(ref_scan, scans_errs, save_dir):
     col_gap = 2
     scan_col_width = 10
     linesep = os.linesep
@@ -16,14 +15,11 @@ def report_corr_err(q, ref_scan, dir_no, save_dir):
     table_title += f'correlation error{linesep}'
     report_table += table_title
 
-    no = dir_no
-    while no > 0:
-        (scan, err) = q.get()
+    for scan, err in scans_errs:
         row = str(scan)
         row += row[0].ljust(scan_col_width + col_gap)
         row += f'{err}{linesep}'
         report_table += row
-        no -= 1
 
     with open(ut.join(save_dir, f'corr_err_{ref_scan}.txt'), 'w+') as f:
         f.write(report_table)
@@ -58,11 +54,6 @@ def read_align(get_scan_func, refarr, scan_node):
 
 
 def combine_scans(get_scan_func, scans_nodes, experiment_dir):
-    results = []
-
-    def collect_result(result):
-        results.append(result)
-
     (refscan, refnode) = scans_nodes.pop(0)
     refarr = get_scan_func(refnode)
 
@@ -70,31 +61,52 @@ def combine_scans(get_scan_func, scans_nodes, experiment_dir):
     # array on gpu memory. Setting library here before starting multiple processes
     dvut.set_lib_from_pkg('np')
 
-    # start reporting process. It will get correlation error for each scan with reference
-    # to the refarray. It will receive the errors via queue.
-    q = Queue()
-    p = Process(target=report_corr_err, args=(q, refscan, len(scans_nodes), experiment_dir))
-    p.start()
-
-    nproc = min(len(scans_nodes), os.cpu_count() * 2)
-
     sumarr = np.zeros_like(refarr)
     sumarr = sumarr + refarr
+    scans_errs = []
+    for scan_node in scans_nodes:
+        ar, er, scan = read_align(get_scan_func, refarr, scan_node)
+        scans_errs.append((scan, er))
+        sumarr = sumarr + ar
 
-    func = partial(read_align, get_scan_func, refarr)
-    with Pool(processes=nproc) as pool:
-        pool.map_async(func, scans_nodes, callback=collect_result)
-        pool.close()
-        pool.join()
-        pool.terminate()
-
-    if len(results) > 0:
-        for res in results[0]:
-            [ar, er, scan] = res
-            sumarr = sumarr + ar
-            q.put((scan, er))
-    else:
-        print(f'did not find any scans to align with {refscan}')
+    report_corr_err(refscan, scans_errs, experiment_dir)
+    # results = []
+    #
+    # def collect_result(result):
+    #     results.append(result)
+    #
+    # (refscan, refnode) = scans_nodes.pop(0)
+    # refarr = get_scan_func(refnode)
+    #
+    # # It is faster to run concurrently on cpu than on gpu which needs uploading
+    # # array on gpu memory. Setting library here before starting multiple processes
+    # dvut.set_lib_from_pkg('np')
+    #
+    # # start reporting process. It will get correlation error for each scan with reference
+    # # to the refarray. It will receive the errors via queue.
+    # q = Queue()
+    # p = Process(target=report_corr_err, args=(q, refscan, len(scans_nodes), experiment_dir))
+    # p.start()
+    #
+    # nproc = min(len(scans_nodes), os.cpu_count() * 2)
+    #
+    # sumarr = np.zeros_like(refarr)
+    # sumarr = sumarr + refarr
+    #
+    # func = partial(read_align, get_scan_func, refarr)
+    # with Pool(processes=nproc) as pool:
+    #     pool.map_async(func, scans_nodes, callback=collect_result)
+    #     pool.close()
+    #     pool.join()
+    #     pool.terminate()
+    #
+    # if len(results) > 0:
+    #     for res in results[0]:
+    #         [ar, er, scan] = res
+    #         sumarr = sumarr + ar
+    #         q.put((scan, er))
+    # else:
+    #     print(f'did not find any scans to align with {refscan}')
 
     return sumarr
 
@@ -108,6 +120,5 @@ def process_batch(get_scan_func, scans_nodes, save_file, experiment_dir):
     save_dir = os.path.dirname(save_file)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    # print(f"Saving array (max={int(arr.max())}) as {save_dir + '/' + filename}")
     ut.save_tif(arr, save_file)
 
