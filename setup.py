c577140c68d2c27069d635ca6059ca6bdfa8b393
bfrosik <bfrosik@anl.gov>
2025-06-11 16:14:13 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import setuptools
 
 cur_dir = os.getcwd().replace(os.sep, '/')
 
@@ -31,3 +32,30 @@ with open('example_workspace/esrf_exp_4/conf/config', 'r') as file :
 filedata = filedata.replace('CUR_DIR', cur_dir)
 with open('example_workspace/esrf_exp_4/conf/config', 'w') as file:
     file.write(filedata)
+
+setuptools.setup(
+      name='cohere_ui',
+      author = 'Barbara Frosik, Ross Harder',
+      author_email = 'bfrosik@anl.gov',
+      url='https://github.com/advancedPhotonSource/cohere/cohere-ui',
+      version='4.2.0',
+      packages=['cohere_ui', 
+                'cohere_ui.api', 
+                'cohere_ui.beamlines.aps_1ide', 
+                'cohere_ui.beamlines.aps_34idc', 
+                'cohere_ui.beamlines.esrf_id01', 
+                'cohere_ui.beamlines.Petra3_P10', 
+                'cohere_ui.beamlines.simple'],
+      install_requires=[
+                         'pyqt5',
+                         'scikit-image',
+                         'xrayutilities',
+                         'pyvista',
+                         'scipy==1.14.1',
+                        ],
+      classifiers=[
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+      ],
+)
